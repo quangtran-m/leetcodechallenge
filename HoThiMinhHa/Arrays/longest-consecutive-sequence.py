765b019fb1c00e3b014d58b19683fe01937700e5
tmhho <amiho@Hos-MacBook-2.local>
2020-04-29 18:12:16 +0200

    Ha push 29/4

---
+++ HoThiMinhHa/Arrays/longest-consecutive-sequence.py
@@ -0,0 +1,27 @@
+#https://leetcode.com/problems/longest-consecutive-sequence/
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        set_nums= set(nums)
+        longest = 0
+        for num in set_nums: 
+            if num - 1 not in set_nums:
+                current_num = num
+                count = 1 
+                while current_num + 1 in set_nums: 
+                    count += 1
+                    current_num += 1
+                longest = max(longest,count)        
+        return longestclass Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        set_nums= set(nums)
+        longest = 0
+        current_num = 0
+        if len(nums) > 0:
+            for num in set_nums: 
+                if current_num - 1 not in set_nums:
+                    current_num = num
+                    count = 1 
+                    while current_num + 1 in set_nums: 
+                        count += 1 
+                    longest = max(longest,count)        
+        return longest
